65db6f7da075a176a1624abc5a5d836f3ad6cb98
Espen Helgedagsrud <espenoh@ifi.uio.no>
2012-05-01 21:27:21 +0200

    Code generation of IfStmt. Dunno if it works properly yet.. :D

--- src/syntaxtree/IfStmt.java
+++ src/syntaxtree/IfStmt.java
@@ -1,5 +1,9 @@
 package syntaxtree;
 import java.util.List;
+import bytecode.CodeFile;
+import bytecode.CodeStruct;
+import bytecode.CodeProcedure;
+import bytecode.instructions.*;
 
 public class IfStmt extends Stmt {
 
@@ -12,6 +16,51 @@ public class IfStmt extends Stmt {
     this.if_stmt = if_stmt;
     this.else_stmt = else_stmt;
   }
+
+  @Override
+  public void generateCode(CodeFile file, CodeStruct struct, CodeProcedure proc){
+    System.out.println("If statement");
+    int jumpinstr1 = 0, jumpinstr2 = 0; // We might need 2 jump offsets.
+    
+    //Generate code for exp
+    exp.generateCode(file, null, proc);
+
+    // NOTE: We expect true/false on stack after this! (do we need to generate it?)
+    // We need a jump here if the expr is false, but it needs a value we don't 
+    // have yet. We'll add it later using replaceInstruction.
+    jumpinstr1 = proc.addInstruction(new NOP());
+    
+    if(if_stmt != null){
+	    for(Stmt s : if_stmt){
+        s.generateCode(file, null, proc);	  
+	    }
+	    // If there is an else-part this instruction gets replaced with a jump 
+	    // to the end of the if-block.
+	    jumpinstr2 = proc.addInstruction(new NOP()); 
+	  }
+	  
+	  // Label for the false/else part of the IF. 
+    int falsepos = proc.addInstruction(new NOP()); // Create label here
+    
+    // We inject a conditional jump here into the jumpinstr1 index
+    proc.replaceInstruction(jumpinstr1, new JMPFALSE(falsepos));
+    
+    // ELSE starts here
+	  if(else_stmt != null){
+	    for(Stmt s : else_stmt){
+	      s.generateCode(file, null, proc);
+	    }
+	    
+	   	// Label for the end of the IF. 
+      int endpos = proc.addInstruction(new NOP()); // Create label here
+    
+      // We inject a unconditional jump here into the jumpinstr2 index
+      proc.replaceInstruction(jumpinstr2, new JMP(endpos));
+	  }
+    
+  }
+	
+
 
     public String semanticChecker(SymbolTable table, String type){
 	SymbolTable eres;
